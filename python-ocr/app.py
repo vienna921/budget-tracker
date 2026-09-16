e360235140fc704370a30285319dbb95ca49d658
Vienna Tan <vvtan@ucdavis.edu>
2026-09-16 16:12:06 +0800

    Prepare OCR service for Render

--- python-ocr/app.py
+++ python-ocr/app.py
@@ -163,4 +163,6 @@ def home():
     return jsonify(receipt_data)
 
 if __name__ == "__main__":
-    app.run(port=5001)
+    import os
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port)
