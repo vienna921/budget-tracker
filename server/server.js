dee31248987638940c69953cf796f43c8af1bd7d
Vienna Tan <vvtan@ucdavis.edu>
2026-09-16 13:10:12 +0800

    Use Render port in production

--- server/server.js
+++ server/server.js
@@ -46,7 +46,7 @@ app.use(session({
     }
 }))
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.post("/api/signup", async (req, res) => {
     const { username, password } = req.body
