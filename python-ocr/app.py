1a99f33c77302a87572e47eba04170deacb199d7
Vienna Tan <vvtan@ucdavis.edu>
2026-09-17 06:55:38 +0800

    Log OCR resized image size

--- python-ocr/app.py
+++ python-ocr/app.py
@@ -96,6 +96,8 @@ def ocr():
         )
     else:
         resized_image = image
+        
+    print("OCR SIZE:", resized_image.shape, flush=True)
 
     gray_image = cv2.cvtColor(
         resized_image,
