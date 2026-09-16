aae34b63269abf22fee88bef2ab531d0a2d4448e
Vienna Tan <vvtan@ucdavis.edu>
2026-09-16 11:28:15 +0800

    Connect backend to PostgreSQL

--- server/database.js
+++ server/database.js
@@ -1,75 +1,15 @@
-// get database tool
-const Database = require("better-sqlite3")
-// open SQLite database called budget.db
-const db = new Database("budget.db")
+// loads PostgreSQL's connection pool from the pg package
+const { Pool } = require("pg")
 
-// id INTEGER PRIMARY KEY AUTOINCREMENT = database generates IDs for us
-db.exec(`
-    CREATE TABLE IF NOT EXISTS transactions (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        type TEXT NOT NULL,
-        amount REAL NOT NULL,
-        category TEXT NOT NULL,
-        description TEXT NOT NULL
-    )
-`)
+const { connectionString } = require("pg/lib/defaults")
 
-db.exec(`
-    CREATE TABLE IF NOT EXISTS budgets (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        month TEXT NOT NULL,
-        category REAL NOT NULL,
-        amount TEXT NOT NULL,
-        user_id INTEGER,
-        UNIQUE(user_id, month, category COLLATE NOCASE)
-    )
-`)
+// loads variables from .env
+require("dotenv").config()
 
-db.exec(`
-    CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username TEXT NOT NULL UNIQUE,
-        password TEXT NOT NULL
-    )    
-`)
+// connect Express server and Neon
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+})
 
-// what columns currently exist in transactions table
-// database migration
-const columnExists = db.prepare(`
-        SELECT COUNT(*) AS count
-        FROM pragma_table_info('transactions')
-        WHERE name = 'date'
-    `).get()
-if (columnExists.count === 0) {
-    db.exec(`
-        ALTER TABLE transactions
-        ADD COLUMN date TEXT
-    `)
-}
-
-const userIdColumnExists = db.prepare(`
-    SELECT COUNT (*) AS count
-    FROM pragma_table_info('transactions')
-    WHERE name = 'user_id'
-`).get()
-if (userIdColumnExists.count === 0) {
-    db.exec(`
-        ALTER TABLE transactions
-        ADD COLUMN user_id INTEGER    
-    `)
-}
-
-const budgetUserIdColumnExists = db.prepare(`
-    SELECT COUNT(*) AS count
-    FROM pragma_table_info('budgets')
-    WHERE name = 'user_id'    
-`).get()
-if (budgetUserIdColumnExists.count === 0) {
-    db.exec(`
-        ALTER TABLE budgets
-        ADD COLUMN user_id INTEGER
-    `)
-}
-
-// allow other backend files to use database
-module.exports = db
+// let's server.js use connection
+module.exports = pool
