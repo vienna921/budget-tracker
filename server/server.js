f6f26399d93b72d6f7556980ae30f071273bd165
Vienna Tan <vvtan@ucdavis.edu>
2026-09-17 12:18:31 +0800

    revert

--- server/server.js
+++ server/server.js
@@ -51,7 +51,7 @@ app.use(session({
         httpOnly: true,
         // don't send login cookie when requested from different website
         // protect against Cross-Site Request Forgery (CSRF)
-        sameSite: "none",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         secure: process.env.NODE_ENV === "production"
     }
 }))
